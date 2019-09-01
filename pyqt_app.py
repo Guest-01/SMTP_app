08b03d7525e9fe7aebca5172a3d9e48e50034bda
fromLaptop <cloudonly21@gmail.com>
2019-09-01 19:37:00 +0900

    add menubar and info dialog

--- pyqt_app.py
+++ pyqt_app.py
@@ -2,15 +2,27 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from smtplib import SMTP, SMTP_SSL
-from email.mime.text import MIMEText
+from email.message import EmailMessage
 
 form_class = uic.loadUiType("SMTP_pyqt.ui")[0]
+form_class_2 = uic.loadUiType("ProgramInfoDialog.ui")[0]
+
+class InfoDialog(QDialog,form_class_2):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.pushButton_2.clicked.connect(self.send_button)
+        self.action_2.triggered.connect(self.prog_info)
+        self.action_3.triggered.connect(qApp.quit)
+
+    def prog_info(self):
+        dlg = InfoDialog()
+        dlg.exec_()
     
     def send_button(self):
         self.pushButton_2.setEnabled(False)
@@ -52,12 +64,13 @@ class MyWindow(QMainWindow, form_class):
     
         server.login(mydict['id'], mydict['pw'])  # Strings
 
-        msg = MIMEText(mydict['content'], _charset='euc-kr')
+        msg = EmailMessage()
         msg['Subject'] = mydict['subject']
         msg['From'] = mydict['from']
         msg['To'] = mydict['to']
+        msg.set_content(mydict['content'])
 
-        server.sendmail(mydict['from'], mydict['to'], msg.as_string())
+        server.send_message(msg)
         server.quit()
     
         return
